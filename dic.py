a81336f5687a15eab6838bfc42ddfd51b1cd3999
shrinihegde <shrinihegdeknsr97@gmail.com>
2019-01-11 11:30:33 +0530

    dict search

---
+++ dic.py
@@ -0,0 +1,10 @@
+mydic={1:"speckbit",2:"world",3:"quiet"}
+a=str(input("enter the required name: "))
+b=list(mydic.values())
+print(b)
+if a in b:
+    print('true')
+else:
+    print('false')
+
+        
