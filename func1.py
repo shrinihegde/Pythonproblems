35c245966b2ba9d4dbeea42960958c8c2e6f1133
shrinihegde <shrinihegdeknsr97@gmail.com>
2019-01-12 10:19:11 +0530

    function to add names to the list

--- func1.py
+++ func1.py
@@ -1,8 +1,12 @@
+#function to add the names to the list
 def add(name):
+    #create an empty list
     lst=[]
+    #using the string format split the names and add it to the list
     lst=name.split()
     print(lst)
-
+#get the input name to be added
 a=input("enter the name to be appended: ")
+#function call
 add(a)
 
